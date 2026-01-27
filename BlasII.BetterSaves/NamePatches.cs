ef052be0b1a5c992e85aac5fe664f46abd153ac0
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-27 18:37:47 -0500

    names: fail to load name

--- BlasII.BetterSaves/NamePatches.cs
+++ BlasII.BetterSaves/NamePatches.cs
@@ -1,7 +1,28 @@
-﻿using Il2CppTGK.Game.Components.UI;
+﻿using BlasII.ModdingAPI;
 using HarmonyLib;
+using Il2CppSystem.Threading.Tasks;
+using Il2CppTGK.Game.Components.UI;
+using Il2CppTGK.Map;
 
 namespace BlasII.BetterSaves;
+
+/// <summary>
+/// Updates the SlotInfo with randomizer data
+/// </summary>
+[HarmonyPatch(typeof(MainMenuWindowLogic), nameof(MainMenuWindowLogic.GetSlotInfo))]
+class MainMenuWindowLogic_GetSlotInfo_Patch
+{
+    public static void Postfix(int index, Task<SlotInfo> __result)
+    {
+        //ModLog.Info($"Replacing slot info for slot {index}");
+
+        //__result.Result.currentZone = new ZoneID()
+        //{
+        //    id = __result.Result.currentZone.id,
+        //    name = Main.BetterSaves.CurrentSlotName,
+        //};
+    }
+}
 
 /// <summary>
 /// Display the name and time last played on the slot UI
@@ -11,7 +32,7 @@ class UISelectableMainMenuSlot_SetSlotData_Patch
 {
     public static void Postfix(UISelectableMainMenuSlot __instance, SlotInfo info)
     {
-        string name = Main.BetterSaves.CurrentSlotName;
+        string name = info.currentZone.name;
         string date = info.dateTime.ToString("MMM d yyyy");
         __instance.zoneName.SetText($"{name}    - {date} -");
     }
