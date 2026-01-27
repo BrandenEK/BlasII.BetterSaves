200c306db6e5b8c3d6899202952b84f165cdab8b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-27 11:40:55 -0500

    init: add base project files

---
+++ BlasII.BetterSaves/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.BetterSaves;
+
+internal class Main : MelonMod
+{
+    public static BetterSaves BetterSaves { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        BetterSaves = new BetterSaves();
+    }
+}
---
+++ BlasII.BetterSaves/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.BetterSaves;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.BetterSaves.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
