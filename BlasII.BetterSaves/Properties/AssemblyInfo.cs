36a572854c8190bd0242fee92a96c1bf351db850
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-27 11:49:29 -0500

    core: finish project setup (#1)
    * core: upgrade api version
    
    * core: finish mod info
    
    * core: add license
    
    * docs: add readme
    
    * core: add workflow

--- BlasII.BetterSaves/Properties/AssemblyInfo.cs
+++ BlasII.BetterSaves/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.BetterSaves;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.BetterSaves.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.1")]
