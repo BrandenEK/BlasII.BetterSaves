0fdb1180bc876a226a63f9a2fae76a73bcab0569
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-27 12:10:39 -0500

    core: change slot number text

--- BlasII.BetterSaves/BetterSaves.cs
+++ BlasII.BetterSaves/BetterSaves.cs
@@ -6,8 +6,10 @@ public class BetterSaves : BlasIIMod
 {
     internal BetterSaves() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
+    public int CurrentPage { get; private set; }
+
     protected override void OnInitialize()
     {
-        // Perform initialization here
+        CurrentPage = 1;
     }
 }
---
+++ BlasII.BetterSaves/Patches.cs
@@ -0,0 +1,53 @@
+﻿using BlasII.ModdingAPI;
+using HarmonyLib;
+using Il2CppTGK.Game.Components.UI;
+using UnityEngine;
+
+namespace BlasII.BetterSaves;
+
+//[HarmonyPatch(typeof(UISelectableMainMenuSlot), nameof(UISelectableMainMenuSlot.UpdateSelected))]
+//class UISelectableMainMenuSlot_UpdateSelected_Patch
+//{
+//    public static void Postfix(UISelectableMainMenuSlot __instance)
+//    {
+//        ModLog.Error(__instance.name + ": update");
+
+//        Transform child = __instance.transform.Find("Number");
+
+//        if (child == null)
+//        {
+//            ModLog.Error("Failed to find number transform");
+//            return;
+//        }
+
+//        UIPixelTextWithShadow text = child.GetComponent<UIPixelTextWithShadow>();
+
+//        if (text == null)
+//        {
+//            ModLog.Error("Failed to find text component");
+//            return;
+//        }
+
+//        ModLog.Warn("Text is [" + text.normalText.text + "]");
+//        text.SetText("5");
+//    }
+//}
+
+[HarmonyPatch(typeof(MainMenuWindowLogic), nameof(MainMenuWindowLogic.OnOpenSlots))]
+class MainMenuWindowLogic_OnOpenSlots_Patch
+{
+    public static void Postfix(MainMenuWindowLogic __instance)
+    {
+        ModLog.Error("Opening main menu");
+
+        var list = __instance.slotsList.elementArray;
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int idx = Main.BetterSaves.CurrentPage * 3 + i + 1;
+
+            UIPixelTextWithShadow text = list[i].obj.transform.Find("Number").GetComponent<UIPixelTextWithShadow>();
+            text.SetText(idx.ToString());
+        }
+    }
+}
