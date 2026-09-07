5cfa47605a245ba23ed969dc7cf3cb0152334ca8
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-09-07 09:17:06 -0400

    core: upgrade command to new system

--- BlasII.BetterSaves/BsCommand.cs
+++ BlasII.BetterSaves/BsCommand.cs
@@ -1,44 +1,21 @@
-﻿using BlasII.CheatConsole;
-using System.Linq;
+﻿using BlasII.CheatConsole.Attributes;
+using BlasII.CheatConsole.Commands;
 
 namespace BlasII.BetterSaves;
 
-internal class BsCommand : ModCommand
+internal class BsCommand : ModComplexCommand
 {
     public BsCommand() : base("slotname") { }
 
-    public override void Execute(string[] args)
+    [SubCommand]
+    private void Set(string name)
     {
-        switch (args[0])
-        {
-            case "set":
-                {
-                    SetName(string.Join(' ', args.Skip(1)));
-                    break;
-                }
-            case "clear":
-                {
-                    if (!ValidateParameterCount(args, 1))
-                        return;
-
-                    ClearName();
-                    break;
-                }
-            default:
-                {
-                    WriteFailure("Unknown subcommand: " + args[0]);
-                    break;
-                }
-        }
-    }
-
-    private void SetName(string name)
-    {
-        Write($"Setting slot name to \"{name}\"");
+        Write($"Setting slot name to '{name}'");
         Main.BetterSaves.UpdateSlotName(name);
     }
 
-    private void ClearName()
+    [SubCommand]
+    private void Clear()
     {
         Write("Clearing slot name");
         Main.BetterSaves.UpdateSlotName(null);
